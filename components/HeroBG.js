ffee545c62e90c2e03bd93939f8603a5b2d8480f
m4nuC <m4nu@hotmail.fr>
2021-11-30 18:31:24 +0100

    Integrate background

---
+++ components/HeroBG.js
@@ -0,0 +1,12 @@
+export default function HeroBG() {
+  return (
+    <div className="relative pt-6 pb-16 sm:pb-24">
+      <div className="header-top-bg">
+        <div className="blue"></div>
+      </div>
+      <div className="header-top-img">
+        <img src="/old-lady-bg.png" alt="" />
+      </div>
+    </div>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
+import SweaterCarousel from '@components/SweaterCarousel'
+import HeroBG from '@components/HeroBG'
 
 export default function Home() {
   return (
-    <div className="container">
+    <div>
       <Head>
         <title>Next.js Starter!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header/>
+        <SweaterCarousel/>
+        <HeroBG/>
         <p className="description">
           Get started by editing <code>pages/index.js</code>
         </p>
